3e5ad8022f65ceda7368f6be138f07569f528406
wyp <935391095@qq.com>
2025-05-11 15:28:35 +0800

    添加了饿汉模式写法

--- main.cpp
+++ main.cpp
@@ -36,42 +36,75 @@
 
 // 线程安全的懒汉单例
 /*************************************************************/
+//class singleClass {
+//public:
+//    static singleClass *GetInstance() {
+//        if (instance == nullptr) { // 第一次检查
+//            std::unique_lock<std::mutex> ul(_mtx);
+//            if (instance == nullptr) { // 第二次检查
+//                instance = new singleClass();
+//            }
+//        }
+//        return instance;
+//    }
+//
+//private:
+//    static singleClass *instance;
+//    static std::mutex _mtx;
+//    singleClass() = default;
+//    singleClass(const singleClass &s) = default;
+//    singleClass &operator=(const singleClass &s) = default;
+//};
+//
+//singleClass *singleClass::instance = nullptr;
+//std::mutex singleClass::_mtx;
+//
+//int main() {
+//    std::mutex mtx;
+//    std::vector<std::thread> threads;
+//    for (int i = 0; i < 10; ++i) {
+//        threads.push_back(std::thread([i, &mtx]() {
+//            singleClass *object = singleClass::GetInstance();
+//            std::unique_lock<std::mutex> ul(mtx);
+//            std::cout << "线程" << i << "的object地址：" << object << std::endl;
+//        }));
+//    }
+//
+//    for (auto &t: threads) {
+//        t.join();
+//    }
+//    return 0;
+//}
+/*************************************************************/
+
+// 饿汉模式（不管用不用到，都构造出来，本身就是线程安全的）
 class singleClass {
 public:
     static singleClass *GetInstance() {
-        if (instance == nullptr) { // 第一次检查
-            std::unique_lock<std::mutex> ul(_mtx);
-            if (instance == nullptr) { // 第二次检查
-                instance = new singleClass();
-            }
-        }
-        return instance;
+        static singleClass instance;
+        return &instance;
     }
 
+    singleClass(const singleClass &s) = delete;
+    singleClass &operator=(const singleClass &s) = delete;
 private:
-    static singleClass *instance;
-    static std::mutex _mtx;
     singleClass() = default;
-    singleClass(const singleClass &s) = default;
-    singleClass &operator=(const singleClass &s) = default;
 };
 
-singleClass *singleClass::instance = nullptr;
-std::mutex singleClass::_mtx;
-
 int main() {
-    std::mutex mtx;
     std::vector<std::thread> threads;
-    for (int i = 0; i < 10; ++i) {
-        threads.push_back(std::thread([i, &mtx]() {
-            singleClass *object = singleClass::GetInstance();
-            std::unique_lock<std::mutex> ul(mtx);
-            std::cout << "线程" << i << "的object地址：" << object << std::endl;
-        }));
+    std::mutex mtx;
+    size_t num_of_thread = 10;
+    threads.reserve(num_of_thread);
+    for (int i = 0; i < num_of_thread; ++i) {
+        threads.emplace_back([i, &mtx]() {
+            std::lock_guard<std::mutex> lg(mtx);
+            std::cout << "线程" << i << "的instance：" << singleClass::GetInstance() << std::endl;
+        });
     }
-
     for (auto &t: threads) {
         t.join();
     }
+
     return 0;
 }
